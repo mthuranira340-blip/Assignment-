1f76c4a97f6fa52171c98d50b24d12d5f4d747cc
mthuranira340-blip <mthuranira340@gmail.com>
2025-09-18 10:58:54 +0300

    Add files via upload

---
+++ my profile.cpp
@@ -0,0 +1,33 @@
+// simple c program
+/* 
+name : Mark Thuranira
+reg : ct100/g/26254/25
+des : stating my profile
+*/
+#include <stdio.h>
+
+//mainfunction
+
+int main()
+{
+    int height;
+    int contact;
+    int weight;
+    int age;
+
+    printf("enter your height:");
+    scanf("%d", &height);
+    printf("your height is %d \n", height);
+
+    printf("enter your contact:");
+    scanf("%d", &contact);
+    printf("your contact  is %d \n", contact);
+
+    printf("enter your weight");
+    scanf("%d", &weight);
+    printf("your weight is %d \n", weight);
+
+    printf("enter your age:");
+    scanf("%d", &age);
+    printf("your age is %d \n", age);
+}
