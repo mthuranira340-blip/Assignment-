f14bd74a83a16fdf79fc98777fb37220be4a42b0
mthuranira340-blip <mthuranira340@gmail.com>
2025-09-21 14:01:01 +0300

    Create Bank loan eligibility check.c

---
+++ Bank loan eligibility check.c
@@ -0,0 +1,28 @@
+/*
+name mark Thuranira
+reg ct100/g/26254/25
+des  checking for eligibilitybof a bank loan
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int age;
+	float income;
+	
+	
+	printf("enter your age:");
+	scanf("%d", &age);
+	
+	printf("\n enter your annual income(in shillings):");
+	scanf("%f",&income);
+	
+	if(age >=21 && income >=21000){
+	printf("\n congratulations you are eligible for a loan");
+	}
+	else{
+	printf("\n unfortunately we are unable to offer you a loan at this time");
+	}
+	return 0;
+ }
