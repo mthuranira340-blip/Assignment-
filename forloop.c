b5acd4a7f9f05d2d1931943b0332aa582f8dcc4b
mthuranira340-blip <mthuranira340@gmail.com>
2025-10-09 09:37:40 +0300

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,17 @@
+/*
+Name Mark Thuranira
+Reg no ct100/g/26254/25
+Des printing numbers in descending order from 100 to 50
+*/
+
+#include <stdio.h>
+int main()
+{
+	int j;
+	//loop descending from 100 to 50 use for
+	for(j=100; j>=50; j--){
+		//print each number on a new line
+		printf("%d\n",j);
+	}
+	return 0;
+}
